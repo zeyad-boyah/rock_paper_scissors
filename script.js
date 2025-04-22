59fafdf89c9dde736b6b113de0fa24d60385973d
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-04-22 09:20:46 +0200

    add intial ui

--- script.js
+++ script.js
@@ -52,11 +52,11 @@ function playGame() {
     }
   }
 
-  for (i = 0; i < 5; i++) {
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
+  // for (i = 0; i < 5; i++) {
+  //   humanSelection = getHumanChoice();
+  //   computerSelection = getComputerChoice();
+  //   playRound(humanSelection, computerSelection);
+  // }
   if (humanScore > computerScore) {
     console.log(`We have a winner!!! your score is ${humanScore}`);
   } else if (humanScore == computerScore) {
@@ -66,5 +66,11 @@ function playGame() {
   }
 }
 
-alert("please f12 or open the console")
+
 playGame()
+
+// For now, remove the logic that plays exactly five rounds.
+// Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+// Add a div for displaying results and change all of your console.logs into DOM methods.
+// Display the running score, and announce a winner of the game once one player reaches 5 points.
+// You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
